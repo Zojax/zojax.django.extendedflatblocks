50cb39cd11bade7c2b899ea3ef969dde72c982d0
bubenkoff <bubenkoff@65ff189f-35ee-0310-953c-23049635c3cf>
2010-12-17 00:19:38 +0000

    unicode name for extension

--- src/zojax/django/extendedflatblocks/models.py
+++ src/zojax/django/extendedflatblocks/models.py
@@ -40,4 +40,4 @@ class FlatBlockExtension(models.Model):
         verbose_name_plural = _(u"Flat block extensions")
         
     def __unicode__(self):
-        return '-'.join(self.container.slug, self.flatblock.slug, self.position, self.id)
+        return '-'.join((self.container.slug, self.flatblock.slug, self.position, self.id))
